8dc0463f6f41ac923b6f4bb340dc3575ac3ee9f6
glaxry <101242299+glaxry@users.noreply.github.com>
2023-02-22 22:23:00 +0800

    leetcode27
    同向双指针法

---
+++ 27.cpp
@@ -0,0 +1,13 @@
+//同向双指针法
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); fastIndex++) {
+            if (val != nums[fastIndex]) {
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
