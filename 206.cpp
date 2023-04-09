03729e029a7873d5e6c1e17c8a347f639bde718d
glaxry <101242299+glaxry@users.noreply.github.com>
2023-04-09 21:42:33 +0800

    leetcode206反转链表

---
+++ 206.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head -> next == nullptr){
+            return head;
+        }
+        ListNode *pre = nullptr;
+        ListNode *now = head;
+        ListNode *_next = nullptr;
+        while(now){
+            _next = now -> next;
+            now -> next = pre;
+            pre = now;
+            now = _next;
+        }
+        return pre;
+
+    }
+};
